e858b376ae4199475f44dd72b9d0e8ed4093dc3a
Alessio <alessioroggero@gmail.com>
2026-02-27 14:01:12 +0100

    Aggiunta lezione sulla sintassi

---
+++ sintassi.py
@@ -0,0 +1,22 @@
+# I commenti si scrivono con il simbolo #, tutto quello che segue su quella riga viene ignorato dal programma, come ad esempio questo commento
+
+# In Python non esistono commenti multiriga, 
+# se vuoi scrivere un commento su più righe devi mettere il simbolo # all'inizio di ogni riga, come ho fatto in questo esempio
+# Questo perchè in Python l'andare a capo ha un significato, infatti indica la fine di un'istruzione,
+# per questo se vuoi scrivere un commento su più righe devi mettere il simbolo # all'inizio di ogni riga
+
+# Oltre l'andare a capo, l'identazione è un'altra caratteristica importante di Python,
+# infatti l'indentazione indica il blocco di codice a cui appartiene una determinata istruzione
+#esempio: Le seguenti spaziature indicano che le istruzioni print appartengono al blocco di codice dell'if:
+
+if 1+1==2:
+    print("condizione vera")
+else: 
+    print("condizione falsa")
+
+#Editor di codice di qualità fanno in automatico l'identazione o comunque guidano il programmatore a scrivere codice ben indentato, 
+# ma se stai scrivendo codice in un editor di testo semplice come il blocco note,
+# devi fare attenzione a indentare correttamente il tuo codice, altrimenti otterrai errori di sintassi.
+
+#La giusta spaziatura è di 4 spazi, ma puoi usare anche il tasto tab 1 volta, che in molti editor di codice è configurato per inserire 4 spazigit
+# l'importante è essere coerente e usare sempre lo stesso tipo di spaziatura per indentare il tuo codice.
