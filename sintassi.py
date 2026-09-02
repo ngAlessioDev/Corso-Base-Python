81081d0d5090992f09055862413fc70b4642a110
Alessio <alessioroggero@gmail.com>
2026-09-02 18:53:55 +0200

    Aggiunta lezione sulle variabili

--- sintassi.py
+++ sintassi.py
@@ -18,5 +18,5 @@ else:
 # ma se stai scrivendo codice in un editor di testo semplice come il blocco note,
 # devi fare attenzione a indentare correttamente il tuo codice, altrimenti otterrai errori di sintassi.
 
-#La giusta spaziatura è di 4 spazi, ma puoi usare anche il tasto tab 1 volta, che in molti editor di codice è configurato per inserire 4 spazigit
+#La giusta spaziatura è di 4 spazi, ma puoi usare anche il tasto tab 1 volta, che in molti editor di codice è configurato per inserire 4 spazi
 # l'importante è essere coerente e usare sempre lo stesso tipo di spaziatura per indentare il tuo codice.
